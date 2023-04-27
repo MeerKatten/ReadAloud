0e5fbccc1dcebf0e02fa5acaa355a5ce9e6ef02c
MeerKatten <123115613+MeerKatten@users.noreply.github.com>
2023-04-27 05:24:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const addTextBtn = document.getElementById('add-text-btn');
+const addButton = document.getElementById('add-text-btn');
+const clearTextButton = document.getElementById('clear-text-btn');
+const clearFormButton = document.getElementById('clear-form-btn');
+const pasteButton = document.getElementById('paste-btn');
 const textArea = document.getElementById('text');
 const textOutput = document.getElementById('text-output');
 
-addTextBtn.addEventListener('click', () => {
+addButton.addEventListener('click', () => {
   const text = textArea.value;
-  textOutput.innerHTML += `<p>${text}</p>`;
+  const paragraph = document.createElement('p');
+  paragraph.textContent = text;
+  textOutput.appendChild(paragraph);
+});
+
+clearTextButton.addEventListener('click', () => {
+  const paragraphs = textOutput.querySelectorAll('p');
+  paragraphs.forEach(paragraph => paragraph.remove());
+});
+
+clearFormButton.addEventListener('click', () => {
+  textArea.value = "";
+});
+
+pasteButton.addEventListener('click', () => {
+  navigator.clipboard.readText()
+    .then((text) => {
+      textArea.value = text;
+    })
+    .catch((err) => {
+      console.error('Failed to read clipboard contents: ', err);
+    });
 });
